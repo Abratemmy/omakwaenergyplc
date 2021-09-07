29618d89e55afcc8b1bdea116388fa26e96f5199
temitope adeniyi <temitopeopeyemi2016@gmail.com>
2021-09-07 17:34:02 +0100

    Updates

---
+++ precache-manifest.3c09364cb5159b753bbf282bbfce531f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7500cb0b80aca191baaa",
+    "url": "/omakwaenergyplc/static/css/main.fe134d06.chunk.css"
+  },
+  {
+    "revision": "7500cb0b80aca191baaa",
+    "url": "/omakwaenergyplc/static/js/main.c50a34f5.chunk.js"
+  },
+  {
+    "revision": "054a8cdcc22aaab90410",
+    "url": "/omakwaenergyplc/static/js/runtime~main.bc681428.js"
+  },
+  {
+    "revision": "2b4ee8816bbda94fc5eb",
+    "url": "/omakwaenergyplc/static/css/2.15472c01.chunk.css"
+  },
+  {
+    "revision": "2b4ee8816bbda94fc5eb",
+    "url": "/omakwaenergyplc/static/js/2.6ab75c7e.chunk.js"
+  },
+  {
+    "revision": "ec6970c0e075ee3a5a788ca3d4eb0583",
+    "url": "/omakwaenergyplc/static/media/logo.ec6970c0.svg"
+  },
+  {
+    "revision": "728bc4dc1f0ec1de2887d5e654d0d51c",
+    "url": "/omakwaenergyplc/static/media/image3.728bc4dc.jpeg"
+  },
+  {
+    "revision": "3e4d4775a6903d320406a113621b3d2d",
+    "url": "/omakwaenergyplc/static/media/project2.3e4d4775.jpeg"
+  },
+  {
+    "revision": "1907eed42c3e71d5e7be405a2b64d7e9",
+    "url": "/omakwaenergyplc/static/media/image1.1907eed4.jpeg"
+  },
+  {
+    "revision": "262f2109d23be69dcd67f8eb7ca11271",
+    "url": "/omakwaenergyplc/static/media/image2.262f2109.jpeg"
+  },
+  {
+    "revision": "cb901e29e504a9d1975eed09e351e997",
+    "url": "/omakwaenergyplc/static/media/media1.cb901e29.jpg"
+  },
+  {
+    "revision": "d32560be60b557f038f76530e8f60a42",
+    "url": "/omakwaenergyplc/static/media/media2.d32560be.jpg"
+  },
+  {
+    "revision": "8be8ab21a8a245e2e0f2859a342b67c2",
+    "url": "/omakwaenergyplc/static/media/media3.8be8ab21.jpg"
+  },
+  {
+    "revision": "23932ce35bbdad2430ef45f7c55da6d1",
+    "url": "/omakwaenergyplc/static/media/media4.23932ce3.jpg"
+  },
+  {
+    "revision": "5d3dd0343791ac4f49fccd9d22b0e926",
+    "url": "/omakwaenergyplc/static/media/media5.5d3dd034.jpg"
+  },
+  {
+    "revision": "45a7dc1e26133b6f68d30ec26c958b4f",
+    "url": "/omakwaenergyplc/static/media/project1.45a7dc1e.jpeg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/omakwaenergyplc/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/omakwaenergyplc/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/omakwaenergyplc/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/omakwaenergyplc/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "03825679f66e14070ffc29ce9367a973",
+    "url": "/omakwaenergyplc/index.html"
+  }
+];
